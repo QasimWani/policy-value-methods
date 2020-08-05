f011170f99126d742ff9c134abbfc3549bd34c7b
Qasim Wani <qasim31wani@gmail.com>
2020-08-04 23:00:25 -0400

    removed seed value

--- DDPG/noise.py
+++ DDPG/noise.py
@@ -8,12 +8,11 @@ import copy
 class OUNoise:
     """Ornstein-Uhlenbeck process."""
 
-    def __init__(self, size, seed, mu=0., theta=0.15, sigma=0.2):
+    def __init__(self, size, mu=0., theta=0.15, sigma=0.2):
         """Initialize parameters and noise process."""
         self.mu = mu * np.ones(size)
         self.theta = theta
         self.sigma = sigma
-        self.seed = random.seed(seed)
         self.reset()
 
     def reset(self):
