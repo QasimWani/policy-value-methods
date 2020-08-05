89a28378cef21772b73fbe44a939c82689930223
Qasim Wani <qasim31wani@gmail.com>
2020-08-05 19:40:08 -0400

    replay buffer added

---
+++ TD3/utils.py
@@ -0,0 +1,48 @@
+import numpy as np
+from collections import deque, namedtuple
+import torch
+
+#Implemented from: https://github.com/QasimWani/policy-value-methods/blob/master/DDPG/ddpg_agent.py#L150
+
+
+BUFFER_SIZE = int(1e6) #max number of experiences in a buffer
+MINI_BATCH = 100 #number of samples to collect from buffer
+
+class ReplayBuffer():
+    """
+    Implementation of a fixed size replay buffer as used in DQN algorithms.
+    The goal of a replay buffer is to unserialize relationships between sequential experiences, gaining a better temporal understanding.
+    """
+    def __init__(self, buffer_size=BUFFER_SIZE, batch_size=MINI_BATCH):
+        """
+        Initializes the buffer.
+        @Param:
+        1. action_size: env.action_space.shape[0]
+        2. buffer_size: Maximum length of the buffer for extrapolating all experiences into trajectories. default - 1e6 (Source: DeepMind)
+        3. batch_size: size of mini-batch to train on. default = 64.
+        """
+        self.action_size = action_size
+        self.replay_memory = deque(maxlen=buffer_size) #Experience replay memory object
+        self.batch_size = batch_size
+        self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"]) #standard S,A,R,S',done
+        
+    def add(self, state, action, reward, next_state, done):
+        """Adds an experience to existing memory"""
+        trajectory = self.experience(state, action, reward, next_state, done)
+        self.replay_memory.append(trajectory)
+    
+    def sample(self):
+        """Randomly picks minibatches within the replay_buffer of size mini_batch"""
+        experiences = random.sample(self.replay_memory, k=self.batch_size)
+
+        states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float().to(device)
+        actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).float().to(device)
+        rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float().to(device)
+        next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float().to(device)
+        dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(device)
+
+        return states, actions, rewards, next_states, dones
+
+    def __len__(self):#override default __len__ operator
+        """Return the current size of internal memory."""
+        return len(self.replay_memory)
